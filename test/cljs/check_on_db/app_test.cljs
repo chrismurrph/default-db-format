6a2c2f7e14c29eab3ddf7db9c7af84913c6f464b
Chris Murphy <chris@strandz.org>
2016-02-06 18:42:25 +1100

    Doesn't matter what you do, boot doesn't work on Windows. About to setup Mint

---
+++ test/cljs/check_on_db/app_test.cljs
@@ -0,0 +1,7 @@
+(ns check-on-db.app-test
+  (:require-macros [cljs.test :refer [deftest testing is]])
+  (:require [cljs.test :as t]
+            [check-on-db.app :as app]))
+
+(deftest test-arithmetic []
+  (is (= (+ 0.1 0.2) 0.3) "Something foul is a float."))
