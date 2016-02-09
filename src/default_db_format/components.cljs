d6bfa7454ae66425faddfae2f197ba11ca358ff1
Chris Murphy <chris@strandz.org>
2016-02-10 07:19:37 +1100

    Nicely displaying the problems

--- src/default_db_format/components.cljs
+++ src/default_db_format/components.cljs
@@ -2,6 +2,16 @@
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(defui OneBad
+  static om/Ident
+  (ident [this props]
+    [:one-bad/by-id (:id props)])
+  Object
+  (render [this]
+    (let [{:keys [bad]} (om/props this)]
+      (dom/li nil (dom/pre nil (apply str bad))))))
+(def one-bad-component (om/factory OneBad {:keyfn :id}))
+
 (defui BadByIds
   static om/Ident
   (ident [this props]
@@ -9,33 +19,19 @@
   Object
   (render [this]
     (let [{:keys [id bads-map]} (om/props this)]
-      (dom/pre nil (apply str bads-map)))))
+      (dom/ul nil
+              (for [bad bads-map]
+                (one-bad-component {:id (first bad) :bad bad}))))))
 (def bad-by-ids-component (om/factory BadByIds {:keyfn :id}))
-
-;#{:line/by-id
-;  {:intersect {:id 302}, :colour {:r 255, :g 0, :b 0}}
-;  :drop-info/by-id
-;  {:x-gas-details [{:id 10100} {:id 10101} {:id 10102}]}}
-;
-;PROPSs 1:
-;{:result {:categories #{graph},
-;          :known-names #{line drop-info graph-point},
-;          :not-normalized #{:line/by-id {:intersect {:id 302}, :colour {:r 255, :g 0, :b 0}}
-;                            :drop-info/by-id {:x-gas-details [{:id 10100} {:id 10101} {:id 10102}]}}}}
 
 (defui DisplayDb
        Object
        (render [this]
                (let [props (om/props this)
-                     _ (println "PROPSs 1:" props)
                      {:keys [result]} props
                      not-normalized (into {} (:not-normalized result))
-                     _ (println "not-normalized: " not-normalized)
                      ]
-                 ;(dom/pre nil (apply str not-normalized))
                  (dom/div nil (for [by-id not-normalized
-                                    :let [_ (println "by-id:" by-id)
-                                          present-lower {:id (first by-id) :bads-map (second by-id)}
-                                          _ (println "present lower:" present-lower)]]
+                                    :let [present-lower {:id (first by-id) :bads-map (second by-id)}]]
                                 (bad-by-ids-component present-lower))))))
 (def display-db-component (om/factory DisplayDb {:keyfn :id}))
