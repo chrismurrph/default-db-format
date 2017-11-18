748dabb9e6e5aa48de055d842d66b824ce78917b
Chris Murphy <chris@seasoft.com.au>
2017-11-18 17:57:05 +0800

    Added watcher so consumer can see changes immediately

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/chrismurrph/default-db-format"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
                  [org.clojure/clojurescript "1.9.854" :scope "provided"]
                  [org.omcljs/om "1.0.0-beta1" :scope "provided"]
+                 ;;[fulcrologic/fulcro "2.0.0-SNAPSHOT" :scope "provided"]
                  [org.clojure/tools.namespace "0.3.0-alpha4"]
                  [org.clojure/core.async "0.3.443"]
                  ]
---
+++ src/default_db_format/watcher.cljs
@@ -0,0 +1,49 @@
+(ns default-db-format.watcher
+  (:require
+    [default-db-format.core :as core]
+    [default-db-format.helpers :as help]
+    [om.next :as om]))
+
+(def bad-result (atom nil))
+
+;;
+;; Intended to be called when Om Next state changes. Stays stuck on the first bad.
+;; (Meaning it caches and keeps returning the first bad)
+;; Returns enough so that caller can see if is first call to return a bad result.
+;; Call subsequent to the first do nothing and furthermore the (cached) result is
+;; not used by watch-state caller function.
+;;
+(defn check!
+  ([config state]
+   (if @bad-result
+     [false @bad-result]
+     [true (reset! bad-result (core/check config state))]))
+  ([state]
+   (check! help/default-config state)))
+
+;;
+;; Un-toggle hold on some/none bad state. After call to this function next call to check!
+;; will actually do a check.
+;;
+(defn un-check! []
+  (reset! bad-result nil))
+
+;;
+;; Changes to state happen frequently. The first problem found here may not end up
+;; as a problem because further mutations will fix the state up later. Hence the
+;; after-1500ms-forced-render does another check, quite likely on different state.
+;;
+(defn watch-state
+  ([config reconciler timeout]
+   (let [timeout (or timeout 1500)
+         state (om/app-state reconciler)]
+     (add-watch state :watcher
+                (fn [key atom old-state new-state]
+                  (let [[first-time? check-result] (check! config new-state)]
+                    (when (and first-time? (not (core/ok? check-result)))
+                      (js/setTimeout (fn []
+                                       (fulcro.client.util/force-render reconciler)
+                                       (un-check!))
+                                     timeout)))))))
+  ([config reconciler]
+    (watch-state config reconciler nil)))
